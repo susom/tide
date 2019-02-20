f9a939aa806c22d44191733f5ebd453c14138fb1
wenchengl <wencheng.li@stanford.edu>
2019-02-20 15:40:33 -0800

    - assert check for GerneralAnonymizer

--- src/test/java/com/github/susom/starr/deid/anonymizers/GeneralAnonymizerTest.java
+++ src/test/java/com/github/susom/starr/deid/anonymizers/GeneralAnonymizerTest.java
@@ -22,18 +22,22 @@ import com.github.susom.starr.deid.anonymizers.GeneralAnonymizer;
 import edu.stanford.irt.core.facade.AnonymizedItem;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GeneralAnonymizerTest {
   private static final Logger log = LoggerFactory.getLogger(GeneralAnonymizerTest.class);
   String[] textArray = new String[]{
     "https://www.yahoo.com/testpage http://www.yahoo.com/testpage https://yahoo.com/testpage www.yahoo.com/testpage",
-    "my phone is 650-876-9087, (650)356-8890 and 1-650-333-4456 ssn test: 345-76-7834 and 234-89-9909-ssn em : wcl@com.com  TEST URL go.com go.gggg and IP: 126.98.0.1 or FE80:0000:0A00:0FE0:0202:B3FF:FE1E:8329, https://www.yahoo.com/testpage ."
+    "my phone is 650-876-9087, (650)356-8890 and 1-650-333-4456 ssn test: 345-76-7834 and 234-89-9909-ssn em : wcl@com.com  TEST URL go.com go.gggg and IP: 126.98.0.1 or FE80:0000:0A00:0FE0:0202:B3FF:FE1E:8329 ."
   };
 
+  int[] phiCount = new int[] {3, 8};
 
   @Before
   public void setUp() throws Exception {
@@ -43,15 +47,37 @@ public class GeneralAnonymizerTest {
   public void scrub() {
 
     GeneralAnonymizer ga = new GeneralAnonymizer();
+
+    IntStream.range(0, textArray.length).forEach(i->{
+      List<AnonymizedItem > items = new ArrayList<>();
+      String result = ga.scrub(textArray[i], "<offset10>" + textArray[i], items);
+      log.info("INPUT:" + textArray[i]);
+      log.info("OUTPUT:" + result);
+
+      for (AnonymizedItem item : items) {
+        String clipFromOriginal = textArray[i].substring(item.getStart(), item.getEnd());
+        log.info(String.format("item words:%s type:%s span from:%s to:%s verify word:%s",
+          item.getWord(), item.getType(), item.getStart(), item.getEnd(), clipFromOriginal));
+        assertEquals("span start and end should be based on original, not on processed text with offset", item.getWord(), clipFromOriginal );
+      }
+
+      assertTrue(items.size() == phiCount[i]);
+    });
+
+
     for(String text : textArray){
       List<AnonymizedItem > items = new ArrayList<>();
       String result = ga.scrub(text, "<offset10>" + text, items);
-      System.out.println("INPUT:" + text);
-      System.out.println("OUTPUT:" + result);
+      log.info("INPUT:" + text);
+      log.info("OUTPUT:" + result);
+
+
+
 
       for (AnonymizedItem item : items) {
         log.info(String.format("item words:%s type:%s span from:%s to:%s verify word:%s",
           item.getWord(), item.getType(), item.getStart(), item.getEnd(), text.substring(item.getStart(), item.getEnd())));
+
       }
     }
 
