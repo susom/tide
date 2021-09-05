bdaf160683664938a85e713b803ddbca6d78903d
Gurpreet Singh <49033319+supersaiyane@users.noreply.github.com>
2021-09-05 17:49:34 +0530

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]
 then
-echo "$ARTIFACT_REGISTRY_KEY" | docker login -u _json_key_base64 --password-stdin https://us-west1-docker.pkg.dev
-export SHRINE_REPO=us-west1-docker.pkg.dev/som-rit-infrastructure-prod/i2b2/shrine
+echo "$DOCKER_API_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin somrit/tide
+export SHRINE_REPO=somrit/tide
 docker build -f Dockerfile -t $SHRINE_REPO:build-$TRAVIS_BUILD_NUMBER .
 echo $SHRINE_REPO:build-$TRAVIS_BUILD_NUMBER
 docker push $SHRINE_REPO
